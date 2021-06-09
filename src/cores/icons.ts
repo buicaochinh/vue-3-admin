a0cb8ed6036731448cf9061f4970c558d6d7983f
Cao Chinh Bùi <buicaochinh0811@gmail.com>
2021-06-09 20:01:07 +0700

    fix icon

---
+++ src/cores/icons.ts
@@ -0,0 +1,3 @@
+export const DASHBOARD_ICON: string = 'el-icon-menu'
+export const DOCUMENT_ICON: string = 'el-icon-document'
+export const USER_ICON: string = 'el-icon-user'
--- src/cores/settings.ts
+++ src/cores/settings.ts
@@ -1,5 +1,6 @@
 import { RouteRecordName } from 'vue-router'
 import { DASHBOARD_NAME, DOCUMENT_NAME, USER_NAME } from '../cores/routes'
+import { DASHBOARD_ICON, DOCUMENT_ICON, USER_ICON } from '../cores/icons'
 
 interface NavItem {
     name: RouteRecordName,
@@ -15,14 +16,14 @@ export const APP_NAME: string = 'Admin'
 export const NAV_ITEM_LIST: NavItemList = [
     {
         name: DASHBOARD_NAME,
-        icon: 'el-icon-menu',
+        icon: DASHBOARD_ICON,
     },
     {
         name: DOCUMENT_NAME,
-        icon: 'el-icon-document',
+        icon: DOCUMENT_ICON,
     },
     {
         name: USER_NAME,
-        icon: 'el-icon-user',
+        icon: USER_ICON,
     },
 ]
